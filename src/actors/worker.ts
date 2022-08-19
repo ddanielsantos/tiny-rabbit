6c549ae258b44917ffeced069d8ddd11efdbcc82
ddanielsantos <contatodanieljob@gmail.com>
2022-08-19 17:27:06 -0300

    feat: add worker

---
+++ src/actors/worker.ts
@@ -0,0 +1,51 @@
+import amqplib from 'amqplib'
+import { sumLine } from '../utils/sum-line'
+import { config } from '../config/config'
+
+import type { Batch, WorkerMessage } from '../types/types'
+
+async function worker() {
+  const queueToListen = 'tasks'
+  const conn = await amqplib.connect(config.RABBIT_URL)
+
+  const consumer = await conn.createChannel()
+
+  await consumer.assertQueue(queueToListen, { durable: true })
+
+  consumer.consume(queueToListen, msg => {
+    if (!msg) {
+      console.log('cancelled by the server')
+      return
+    }
+
+    const messageContent = msg.content.toString()
+
+    if(!messageContent) {
+      return
+    }
+
+    const load: Batch = JSON.parse(messageContent)
+
+    const sum = load.lines.reduce((prev, curr) => {
+      const currentLineSum = sumLine(curr)
+      return currentLineSum + prev
+    }, 0)
+
+    const message: WorkerMessage = {
+      id: load.id,
+      sum,
+      processedLines: load.lines.length,
+      totalLines: load.totalLines
+    }
+    
+    const stringifiedMessage = JSON.stringify(message)
+    console.log(stringifiedMessage)
+
+    const destinationQueue = 'aggregator'
+    consumer.ack(msg)
+    consumer.sendToQueue(destinationQueue, Buffer.from(stringifiedMessage))
+  })
+  
+}
+
+worker()
